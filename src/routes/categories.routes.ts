26d262201d0339f2c537a9bac4ca821c67084451
ludico1959 <ludico1959@gmail.com>
2022-04-02 03:43:38 -0300

    feat: :sparkles: add categories name validation

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,6 +8,15 @@ const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
+  const { name } = request.body;
+
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists)
+    return response.status(400).json({
+      error: 'Category already exists'
+    });
+
   categoriesRepository.create(request.body);
 
   return response.status(201).send();
