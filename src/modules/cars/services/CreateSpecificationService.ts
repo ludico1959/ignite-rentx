14bf881eccdeed13831ab85bc3921555b66cfb96
ludico1959 <ludico1959@gmail.com>
2022-04-03 00:43:20 -0300

    feat: :sparkles: create specification route, service, repository and model

---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,24 @@
+import { ISpecificationsRepository } from '../repositories/ISpecificationsRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationService {
+  private specificationsRepository: ISpecificationsRepository;
+
+  constructor(specificationsRepository: ISpecificationsRepository) {
+    this.specificationsRepository = specificationsRepository;
+  }
+
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists = this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadyExists) throw new Error('Specification already exists!');
+
+    this.specificationsRepository.create({ name, description });
+  }
+}
+
+export { CreateSpecificationService };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { categoriesRoutes } from './routes/categories.routes';
+import { specificationsRoutes } from './routes/specifications.routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/categories', categoriesRoutes);
+app.use('/specifications', specificationsRoutes);
 
 const port = 3333;
 app.listen(port, () => {
